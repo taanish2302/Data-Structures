a518ff22e0526bf15dbd9eee4489fc0895bd5529
taanish2302 <75430328+taanish2302@users.noreply.github.com>
2025-10-28 13:22:19 +0530

    Add files via upload

---
+++ stack.c
@@ -0,0 +1,78 @@
+//Stack
+#include <stdio.h>
+#define size 5
+int stack[size];
+int top=-1;
+
+void push(int value){
+    if(top==size-1){
+        printf("stack overflow\n");
+    }
+    else{
+        top+=1;
+        stack[top]=value;
+    }
+}
+int pop(){
+    if(top==-1){
+        printf("stack underflow\n");
+        return -1;
+    }
+    else{
+        int value=stack[top];
+        top-=1;
+        return value;
+    }
+}
+int peek(){
+    if(top==-1){
+        printf("stack is empty\n");
+        return -1;
+    }
+    else{
+        return stack[top];
+    }
+}
+
+int main(){
+    int ch;
+    while (1){
+        printf("1.push\n2.pop\n3.peek\n4.exit\n");
+        printf("enter your choice:");
+        scanf("%d",&ch);
+        switch (ch){
+            case 1:
+                {
+                    int value;
+                    printf("enter value to push:");
+                    scanf("%d",&value);
+                    push(value);
+                    break;
+                }
+            case 2:
+                {
+                    int value=pop();
+                    if(value!=-1){
+                        printf("popped value:%d\n",value);
+                    }
+                    break;
+                }
+            case 3:
+                {
+                    int value=peek();
+                    if(value!=-1){
+                        printf("top value:%d\n",value);
+                    }
+                    break;
+                }
+            case 4:
+                {
+                    return 0;
+                }
+            default:
+                {
+                    printf("invalid choice\n");
+                }
+        }
+    }
+}
